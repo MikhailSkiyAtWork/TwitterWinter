4574fb9006ad825e17b95b7e10a1877e5da4216d
MikhailSkiyAtWork <mvaluyskiy@weezlabs.com>
2016-01-28 18:11:20 +0300

    Initial Commit

---
+++ TwitterWinter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterWinter
+//
+//  Created by Михаил Валуйский on 28.01.16.
+//  Copyright © 2016 Михаил Валуйский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
